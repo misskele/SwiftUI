e6ce6b68ac0d2084160d41752fb66b91d5e82f4e
likaili <kaili_up@163.com>
2020-06-03 17:35:45 +0800

    tabView + navigationView
    Signed-off-by: likaili <lkl@ngarihealth.com>

--- SwiftUI_TabView+NavigationView/SwiftUI_TabView+NavigationView/ContentView.swift
+++ SwiftUI_TabView+NavigationView/SwiftUI_TabView+NavigationView/ContentView.swift
@@ -10,17 +10,48 @@ import SwiftUI
 
 struct ContentView: View {
     @State var index = 0
+    let imgs = ["hot","recommend","search","tag","setting"]
+    
     var body: some View {
-//        Text("Hello, World!")
         TabView (selection: $index){
-            Text("tabBar")
-                .tabItem {
-                    Text("Bar0")
+            //            ForEach (0..<imgs.count){item in
+            //                NavigationView{
+            //                    Text("navigation\(item)")
+            //                    .navigationBarTitle("Title_\(item)")
+            //                    }
+            //                .tabItem{
+            //                    Image(self.imgs[item])
+            //                    Text("tabItem\(item)")
+            //                }
+            //            .tag(item)
+            //            }
+            NavigationView{
+                ZStack{
+                    Rectangle().foregroundColor(.red)
+                    Text("0")
+                    .navigationBarTitle("Title_0")
+                }
+                
             }
-            Text("tabBar")
-                .tabItem {
-                    Text("Bar1")
+            .tabItem{
+                Image("hot")
+                Text("tabItem0")
             }
+            .tag(0)
+            
+            NavigationView{
+                ZStack{
+                    Rectangle().foregroundColor(.orange)
+                    Text("1")
+                        .navigationBarTitle("Title_1")
+                }
+            }
+            .tabItem{
+                Image("recommend")
+                Text("tabItem1")
+            }
+            .tag(1)
+            
         }
     }
 }
