671f50c06a325e5ebef8fc088e985d30f4a2252d
chenzheiu1210 <chenzhecool@163.com>
2020-06-20 21:55:43 +0800

    state关键字

--- SwiftUI_List/SwiftUIDemo/ContentView.swift
+++ SwiftUI_List/SwiftUIDemo/ContentView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by ng on 2020/5/28.
 //  Copyright © 2020 ngarihealth. All rights reserved.
-//
+//https://www.jianshu.com/p/2713f904a405
 
 import SwiftUI
 
@@ -13,11 +13,15 @@ struct Person {
     let heigh : Int
 }
 struct ContentView: View {
-    
+    //state 关键字
+    //当 State 更新的时候，view将重新校验UI，并且更新自身
+    //从原理上说，我们只要改变了 view 里面被关键词 @State 修饰的属性，
+    //整个 view 的 body 就会被重新渲染。//https://www.jianshu.com/p/aace56356aa7
     @State var dataArr:[Person] = [Person.init(name: "1号", heigh: 170),
-                   Person.init(name: "2号", heigh: 180),
-                   Person.init(name: "3号", heigh: 190)
+                                   Person.init(name: "2号", heigh: 180),
+                                   Person.init(name: "3号", heigh: 190)
     ]
+    @State var showDetails = true
     
     var body: some View {
         NavigationView{
@@ -27,8 +31,16 @@ struct ContentView: View {
                     Text("姓名:" + data.name)
                     Text("身高:" + String(data.heigh)).foregroundColor(.red)
                 }
-            }.navigationBarTitle(Text("表格").foregroundColor(.red), displayMode: .inline)
-                .navigationBarItems(trailing: Button(action: addPerson, label: {Text("Add")}))
+            }.navigationBarTitle(Text("表格"))
+                .navigationBarItems(
+                    leading: Button(action: {self.showDetails.toggle()}, label:{Text(self.showDetails ? "Hide" : "show")}),
+                    trailing: Button(
+                        action: addPerson,
+                        label: {
+                            Text("Add")
+                    }
+                    )
+            )
         }
     }
     func addPerson() {
